fa51e988cfb3eb0ed99bc68c12ae368ffa02356c
Chiyembekezo <phoenixtech2003@gmail.com>
2024-05-05 14:03:54 +0200

    chore: Refactor fetchWeatherDetails.js for better readability

--- src/js/fetchWeatherdetails.js
+++ src/js/fetchWeatherdetails.js
@@ -1,12 +1,14 @@
-const fetchWeatherDetails = function getsWeatherDetailsOfSpecifiedLocation(){
-  const getDetails = async function fecthDetails(location){
-    let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=dffb002748c3494ca8a61940240105&q=${location}`,{mode:'cors'});
+const fetchWeatherDetails = function getsWeatherDetailsOfSpecifiedLocation() {
+  const getDetails = async function fecthDetails(location) {
+    let response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=dffb002748c3494ca8a61940240105&q=${location}`,
+      { mode: "cors" }
+    );
     let data = await response.json();
     return data;
-  }
+  };
 
-  return{getDetails};
-}
+  return { getDetails };
+};
 
 export default fetchWeatherDetails;
-
